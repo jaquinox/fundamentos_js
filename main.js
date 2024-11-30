835dc649355bb0485eea085daa5f9c1b316f18b3
aquino <jaquinox123@gmail.com>
2024-11-30 16:05:03 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,100 @@
+// Variables
+
+// let MyName;
+
+// MyName = "jose fino";
+
+// let MyAge = 15;
+
+// alert(MyName);
+// alert(MyAge);
+
+MyName = "jose fino 2",
+  MyAge = 15,
+  MyMessage = "Hello :)";
+
+
+// alert(MyName);
+// alert(MyAge);
+// alert(MyMessage);
+
+// constantes
+
+const MyBirthDay = "Apr-21-2014";
+
+// alert(MyBirthDay);
+
+
+
+// tipos de datos
+
+
+let MyAge2 = 33;
+
+
+let IsActive = true;
+
+let IsBlue = false;
+
+let MyHeight = null;
+
+let MyHeight2 = undefined;
+
+// ingreso de argumentos
+
+// let Year = prompt('anio atual: ');
+// alert(Year);
+
+
+// let isBoss = confirm('eres el jefe? ');
+
+// alert(isBoss);
+
+// operadores matematicos
+
+let Num1 = 10;
+let Num2 = 20;
+
+// suma
+
+// alert('suma: '+ (Num1+Num2));
+
+// //resta
+// alert('resta: '+(Num1-Num2));
+
+// //multiplicacion
+// alert('multiplicacion: '+(Num1*Num2));
+
+// // division
+
+// alert('division: '+(Num1/Num2));
+
+// // modulo
+
+// alert('modulo: '+(Num1%Num2));
+
+//incremento
+
+let Num3 = 3;
+
+Num3 ++;
+
+// alert(Num3);
+
+// operadores de comparacion
+
+alert (2>1);
+alert (2<1);
+alert (2==1);
+alert (2!=1);
+
+
+
+
+
+
+
+
+
+
+
